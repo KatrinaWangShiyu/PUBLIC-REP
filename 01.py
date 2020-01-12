36dd491d9bb92133973ad30c224380545d109e65
王食欲 <59759021+KatrinaWangShiyu@users.noreply.github.com>
2020-01-12 14:09:00 +0800

    first commit

---
+++ 01.py
@@ -0,0 +1,77 @@
+'''
+python 值类型
+int float string bool
+int - 整数
+
+# int
+a = 5
+print(a)
+
+float - 浮点数  带小数点
+
+# float
+a = 5.0
+print(a)
+
+
+string 字符串
+
+a = 'Hello'
+b = "World"
+
+bool - 布尔
+
+a = True
+b = False
+
+
+判断符
+ == > < >= <= !=
+
+a = 7
+b = 5
+
+if a < b:
+    print('a > b')
+
+
+IsNormal = False
+
+if IsNormal:
+    print('航天器正常')
+
+else:
+    print('航天器不正常')
+
+
+for 循环
+import  numpy as np
+
+a = np.zeros(25)
+a = a.reshape(5,5)
+print(a)
+
+for i in range(0,5):
+    for j in range(0,5):
+        a[i,j] = 1
+        print(a)
+
+ '''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
